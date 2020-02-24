472d4dc14ca0729e19a925113a8cfb96c80137df
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-02-23 17:38:16 -0800

    configuration for the database

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-
 var app = express();
 var PORT = process.env.PORT || 3000
 app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
