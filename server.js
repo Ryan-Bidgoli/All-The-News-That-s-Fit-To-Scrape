9e250f2e44dac3617f077018c313bbb4642f7ed1
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-02-23 17:23:49 -0800

    Set Handlebars as the default templating engine

--- server.js
+++ server.js
@@ -8,3 +8,6 @@ var cheerio = require("cheerio");
 var app = express();
 var PORT = process.env.PORT || 3000
 app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
