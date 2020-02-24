797cd12ea73d798f514a5ae7796e557aa87a3eb5
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-02-24 02:10:06 -0800

    loop through & append the elements

--- public/app.js
+++ public/app.js
@@ -17,6 +17,12 @@ function setActive(selector) {
 $("#headline-sort").on("click", function(){
   $("#tbody").empty();
   setActive("#title");
-
+for (var i = 0; i < data.length; i ++) {
+      $("#tbody").append("<tr><td>" + data[i].title + "</td>" +
+      "<td><a href='" + data[i].link + "'>" + data[i].link + "</a></td>" +
+      "<td><img src='" + data[i].image +  " '/></td>" +
+      "<td>" + data[i].summary + "</td></tr>");
+    }
+  })
  
 });
