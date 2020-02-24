27328e43545334fa1751524a0412761374fd9437
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-02-24 01:13:44 -0800

    appending each of the elements

--- public/app.js
+++ public/app.js
@@ -2,5 +2,8 @@
 $.getJSON("/all", function(data){
    console.log(data);
    for (var i = 0; i < data.length; i++){
+      $("#results").append( "<tr><td>" + data[i].title + "</td>" +
+                            "<td><a href='" + data[i].link + "'>" + data[i].link + "</a></td>" +
+                            "<td><img src='" + data[i].image +  " '/></td>" + 
    }
 });
